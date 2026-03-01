c90639f1717c883e039354dc6a104263c035332f
S1kar1o <144246367+S1kar1o@users.noreply.github.com>
2026-03-02 00:27:12 +0200

    pictures

---
+++ RootMobile/Views/TreeGameContecst/TreeOfUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootMobile.Views.TreeGameContecst
+{
+    public class TreeOfUser
+    {
+        public int lvlOfTree { get; private set; }
+        public int amountOfGrow { get; private set; }
+        public int amountToLvlUp { get; private set; }
+        public string nameOfTree { get; private set; }
+        public string nameOfImage { get; private set; }
+        public float offsetX { get; private set; }
+        public float offsetY { get; private set; }
+        public TreeOfUser()
+        {
+            lvlOfTree = 0;
+            amountOfGrow = 0;
+            amountToLvlUp = 0;
+            nameOfTree = "default";
+            offsetX = 0;
+            offsetY = 0;
+            nameOfImage = "lvl_1.png";
+        }
+    }
+}
