b237f5d4e685b034ed062d52aec1fc3873c66f7a
S1kar1o <144246367+S1kar1o@users.noreply.github.com>
2026-03-02 00:27:12 +0200

    pictures

---
+++ RootMobile/Views/TreeGameContecst/WateringCan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootMobile.Views.TreeGameContecst
+{
+    public class WateringCan
+    {
+        private int amountOfGivenProgress;
+        private int lvl;
+        private float offsetY;
+        private float offsetX;
+        public WateringCan()
+        {
+            amountOfGivenProgress = 0;
+            lvl = 1;
+            offsetX = 0;
+            offsetY = 0;
+        }
+
+    }
+}
--- RootMobile/Views/TreeGameView.xaml.cs
+++ RootMobile/Views/TreeGameView.xaml.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using RootMobile.Views.TreeGameContecst;
 namespace RootMobile.Views;
 
 public partial class TreeGameView : ContentPage
 {
+    private TreeOfUser treeOfUser;
+    private WateringCan wateringCan;
     public TreeGameView()
     {
+        treeOfUser = new TreeOfUser();
+        wateringCan = new WateringCan();
         InitializeComponent();
+        TreeSprite.Source = treeOfUser.nameOfImage;
     }
 }
