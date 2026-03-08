bfd1617488cbadaebda783c6467346c6a0e5e4f8
Valentine <gandribidav@gmail.com>
2026-03-08 23:44:05 +0200

    new account features

--- RootMobile/ViewModels/AccountViewModel.cs
+++ RootMobile/ViewModels/AccountViewModel.cs
@@ -6,23 +6,66 @@ using RootMobile.Views;
 
 namespace RootMobile.ViewModels;
 
-public partial class AccountViewModel  : ObservableObject
+public partial class AccountViewModel : ObservableObject
 {
-    private DataService _dataService;
+    private readonly DataService _dataService;
 
-    [ObservableProperty] 
+    [ObservableProperty]
     private UserDataModel userData;
-    
+
+    [ObservableProperty]
+    private string selected = "trees";
+
+    public bool IsTreesSelected => Selected == "trees";
+    public bool IsBadgesSelected => Selected == "badges";
+
+    [ObservableProperty]
+    private TreesViewModel treesVm = new();
+
+    [ObservableProperty]
+    private BadgesViewModel badgesVm = new();
+
     public AccountViewModel(IDataService dataService)
     {
         _dataService = (DataService)dataService;
-        Initialize();
+        _ = Initialize();
+    }
 
+    partial void OnSelectedChanged(string value)
+    {
+        OnPropertyChanged(nameof(IsTreesSelected));
+        OnPropertyChanged(nameof(IsBadgesSelected));
+    }
+
+    [RelayCommand]
+    private void SelectTab(string tab)
+    {
+        Selected = tab.ToLower();
+
+        if (tab == "trees")
+        {
+            TreesVm = new TreesViewModel(_dataService);
+        }
+        else if (tab == "badges")
+        {
+            BadgesVm = new BadgesViewModel(_dataService);
+        }
+            
+
+    }
+
+    [RelayCommand]
+    private void Swipe(string direction)
+    {
+        if (direction == "left" && Selected == "trees")
+            Selected = "badges";
+        else if (direction == "right" && Selected == "badges")
+            Selected = "trees";
     }
 
     private async Task Initialize()
     {
-        UserData =  await _dataService.GetUserData();
+        UserData = await _dataService.GetUserData();
     }
 
     [RelayCommand]
@@ -30,18 +73,16 @@ public partial class AccountViewModel  : ObservableObject
     {
         await Shell.Current.Navigation.PushAsync(new SettingsView(_dataService));
     }
-    
+
     [RelayCommand]
     private async Task GoToEditAccount()
     {
         await Shell.Current.Navigation.PushAsync(new EditAccountView(_dataService, UserData));
     }
-    
+
     [RelayCommand]
     private async Task GoToEmails()
     {
         await Shell.Current.Navigation.PushAsync(new EmailsView());
     }
-    
-    
 }
